8136a581a40cb82959cf349299b3821824ad86ef
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2020-08-06 12:23:06 +0600

    ES6 practice done

---
+++ destructure.js
@@ -0,0 +1,29 @@
+// const person = {name: 'Jack William', age: 17, job: 'facebooker', gfName: 'Ema Watson', address: 'Kochu Khet', phone: '017181252331', friends: ['tom hancks', 'tom cruise', 'Tom yarn'] }
+
+
+// const { phone, gfName, address, salary } = person;
+// // const gfName = person.gfName;
+// const phone = person.phone;
+
+
+// console.log(gfName, phone, salary, address);
+// console.log(gfName, phone, salary, address);
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+
+// const friends = ['Sakib Khan', 'Arman Khan', 'Amir Khan', 'Salman Khan', 'Sharukh Khan'];
+// const [chotoFriend, nextFriend, ....] = friends;
+// console.log(chotoFriend);
+
+
+const complexObject = {
+    name: 'abc',
+    info: {
+        address: 'kola bagan',
+        leader: 'Tiger Leader'
+    }
+}
+
+const {leader} = complexObject.info;
---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby = "Nosaiba Binte Nadim";
+console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+const nayok = {name:"sakib khan", phone:4578};
+
+// console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
